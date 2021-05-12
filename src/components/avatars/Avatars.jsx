f5188c811ced14c2cb4836dddc1b617be216df3e
Minh Ngo <ngo.minh0224@gmail.com>
2021-05-12 10:31:44 -0700

    avatar.jsx

---
+++ src/components/avatars/Avatars.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Character = ({ name, photoUrl, affiliation }) => {
+  return (
+    <div className="character">
+      <p>{name}</p>
+      <img src={photoUrl} alt={name} />
+      <p>{affiliation}</p>
+    </div>
+  );
+};
+
+Character.propTypes = {
+  name: PropTypes.string.isRequired,
+  photoUrl: PropTypes.string.isRequired,
+  affiliation: PropTypes.string.isRequired,
+}
+
+export default Villager;
